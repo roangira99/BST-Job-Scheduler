bd9a20f02b99d9efdd94bdf23bb029b2c4852470
roangira99 <angirarobert7@gmail.com>
2023-05-18 14:46:38 +0300

    Added file with application interface logic

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ class Node:
     def __str__(self):
         return f"Time: {self.data}, Duration: {self.duration}, End: {self.scheduled_end}, Jobname: {self.name_of_job}"
 
-
 class BSTDemo:
     def __init__(self):
         self.root = None
@@ -87,7 +86,7 @@ class BSTDemo:
             if key == curr.data:
                 return curr # returning the node if found
             elif key < curr.data:
-                return self.find_val(curr.left_child, key)
+                return self._find_val(curr.left_child, key)
             else:
                 return self._find_val(curr.right_child, key)
         return
@@ -99,8 +98,43 @@ class BSTDemo:
             return self.min_right_subtree(curr.left_child)
 
     def delete_val(self, key):
-        pass
+        self._delete_val(self.root, None, None, key)
 
-
-
+    def _delete_val(self, curr, prev, is_left, key):
+        if curr:
+            if key == curr.data:
+                if curr.left_child and curr.right_child:
+                    min_child = self.min_right_subtree(curr.right_child)
+                    curr.data = min_child.data
+                    self._delete_val(curr.right_child, curr, False, min_child.data)
+                elif curr.left_child == None and curr.right_child == None:
+                    if prev:
+                        if is_left:
+                            prev.left_child = None
+                        else:
+                            prev.right_child = None
+                    else:
+                        self.root = None
+                elif curr.left_child == None:
+                    if prev:
+                        if is_left:
+                            prev.left_child = curr.right_child
+                        else:
+                            prev.right_child = curr.right_child
+                    else:
+                        self.root = curr.right_child
+                else:
+                    if prev:
+                        if is_left:
+                            prev.left_child = curr.left_child
+                        else:
+                            prev.right_child = curr.left_child
+                    else:
+                        self.root = curr.left_child
+            elif key < curr.data:
+                self._delete_val(curr.left_child, curr, True, key)
+            elif key > curr.data:
+                self._delete_val(curr.right_child, curr, False, key)
+        else:
+            print(f"{key} not found in Tree")
 
